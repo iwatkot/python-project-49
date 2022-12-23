1a79e13dfd23e3dfde24b230dbe1309b00a21d1f
iwatkot <iwatkot@gmail.com>
2022-12-23 17:40:36 +0300

    Fixed linter problem.

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -23,7 +23,8 @@ def main():
             print('Correct!')
             win_counter += 1
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             game_flag = False
         if win_counter == 3:
